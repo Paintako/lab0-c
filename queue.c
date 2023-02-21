1f2fa46eca862c9b3fcf05eced6117b539316b2d
Eric <p76111652@gs.ncku.edu.tw>
2023-02-21 20:17:36 +0800

    Implementation of node insertion/ deletion
    1. q_insert_head: Malloc a newnode, and this node contains a
    pointer to char,  then malloc sizeof char for string, then
    assign it to char*
    2. q_insert_tail: Same as insert_head, insert in tail instead.
    3. q_remove_head: Remove from head.
    4. q_remove_tail: Remove from tail.
    5. q_delete_mid: Use fast pointer and slow pointer to find mid point,
    then delete it.

--- queue.c
+++ queue.c
@@ -35,30 +35,99 @@ void q_free(struct list_head *l)
     }
     // after removing all nodes in queue, free head pointer
     free(l);
+    return;
 }
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
 {
+    // malloc a spcae for newnode
+    if (!head)
+        return false;
+    element_t *newnode = malloc(sizeof(element_t));
+    if (!newnode) {
+        free(newnode);
+        return false;
+    }
+
+    // +1 for '/0'
+    char *str_ptr = malloc(sizeof(char) * strlen(s) + 1);
+    if (!str_ptr) {
+        free(str_ptr);
+        free(newnode);
+        return false;
+    }
+
+    memcpy(str_ptr, s, strlen(s) + 1);
+    newnode->value = str_ptr;
+    list_add(&newnode->list, head);
     return true;
 }
 
 /* Insert an element at tail of queue */
 bool q_insert_tail(struct list_head *head, char *s)
 {
+    // similar to insert_head
+    element_t *newnode = malloc(sizeof(element_t));
+    if (!newnode) {
+        free(newnode);
+        return false;
+    }
+
+    // +1 for '/0'
+    char *str_ptr = malloc(sizeof(char) * strlen(s) + 1);
+    if (!str_ptr) {
+        free(str_ptr);
+        free(newnode);
+        return false;
+    }
+
+    memcpy(str_ptr, s, strlen(s) + 1);
+    newnode->value = str_ptr;
+    list_add_tail(&newnode->list, head);
     return true;
 }
 
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    // copy remove node's value into sp, and limit sp size < bufsize
+    if (!head || list_empty(head))
+        return NULL;
+    element_t *del_node = list_first_entry(head, element_t, list);
+    list_del(&del_node->list);
+    if (sp) {
+        // when bufsize>1, sp can be assinged with char.
+        for (char *str_ptr = del_node->value; bufsize > 1;
+             sp++, str_ptr++, bufsize--)
+            *sp = *str_ptr;
+        *sp = '\0';  // end of string ends with \0
+    }
+    return del_node;
 }
 
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    // /**
+    //  * since each node has prev and next pointer,
+    //  * we can simply find tail position in queue in O(1)
+    //  */
+    if (!head || list_empty(head))
+        return NULL;
+    element_t *del_node =
+        list_entry(head->prev, element_t, list);  // find entry for remove node
+    if (!del_node)
+        return NULL;
+    list_del(&del_node->list);
+    if (sp) {
+        // when bufsize>1, sp can be assinged with char.
+        for (char *str_ptr = del_node->value; bufsize > 1;
+             sp++, str_ptr++, bufsize--)
+            *sp = *str_ptr;
+        *sp = '\0';  // end of string ends with \0
+    }
+    return del_node;
 }
 
 /* Return number of elements in queue */
@@ -79,13 +148,32 @@ int q_size(struct list_head *head)
 bool q_delete_mid(struct list_head *head)
 {
     // https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/
+    if (!head)
+        return false;
+
+    struct list_head *fast = head->next->next;
+    struct list_head *slow = head->next;
+
+    // after while loop, slow == mid_position
+    while (fast->prev != head && fast != head) {
+        fast = fast->next->next;
+        slow = slow->next;
+    }
+
+    // update queue
+    list_del(slow);
+    element_t *rmv_ptr = list_entry(slow, element_t, list);
+    q_release_element(rmv_ptr);
+
     return true;
 }
 
 /* Delete all nodes that have duplicate string */
 bool q_delete_dup(struct list_head *head)
 {
+    // note:
     // https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
+
     return true;
 }
 
